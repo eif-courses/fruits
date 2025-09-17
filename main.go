d3aee7aba795ee8606abfdc5a4d1e6f136c7d9cb
Markeris <m.gzegozevskis@eif.viko.lt>
2025-09-17 15:35:55 +0300

    update

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 
 	"github.com/eif-courses/fruits/internal/config"
 	restapi "github.com/eif-courses/fruits/internal/handlers"
+	"github.com/jackc/pgx/v5/pgxpool"
+
 	"github.com/eif-courses/fruits/internal/repository"
+	"github.com/eif-courses/fruits/internal/services"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
@@ -52,13 +55,27 @@ func main() {
 	}
 
 	// Now you can use os.Getenv as usual
-	if err := run(os.Getenv("DATABASE_URL")); err != nil {
+	//if err := run(os.Getenv("DATABASE_URL")); err != nil {
+	//	log.Fatal(err)
+	//}
+
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+
+	if err != nil {
 		log.Fatal(err)
 	}
 
+	defer dbpool.Close()
+
+	queries := repository.New(dbpool)
+
+	fruitService := services.NewFruitService(queries)
+
+	fruitHandler := restapi.NewFruitHandler(fruitService)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", restapi.HelloResponse)
+	r.Get("/", fruitHandler.GetFruits)
 
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":8080", r)
 }
